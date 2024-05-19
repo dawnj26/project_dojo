dd8d1bd19a8faf7bf8d39505ed43964ca5b506ef
Joel Miclat Gutlay <144513440+Livsn06@users.noreply.github.com>
2024-05-19 21:25:23 +0800

    Anime card (#1)
    Co-authored-by: Livsn06 <joelgutlau04@gmail.com>

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -15,7 +15,19 @@
         <div class="flex justify-center flex-1 gap-2">
             <livewire:create-nav-bar />
             <div class="flex-1 p-6">
-                <h2 class="text-3xl font-medium">Watching</h2>
+                <h2 class="text-3xl font-medium mb-5">Watching</h2>
+
+                <div class="flex max-w-full flex-wrap *:ml-4 *:mb-4 *:basis-1/11 *:flex-shrink-0 *:rounded-lg *:shadow-xl">
+
+                    <div class=" relative">
+                        <img class=" min-w-40 w-44 rounded-lg" src="https://a.storyblok.com/f/178900/1484x2100/fff351971f/kaiju-no8-jp-kv.jpeg/m/filters:quality(95)format(webp)" alt="image">
+                        <div class=" absolute flex flex-col top-0 rounded-lg bg-gradient-to-b from-transparent to-black bg w-full h-full p-3">
+                            <span class=" w-min bg-red-700 text-white font-light text-xs p-1 rounded-md shadow-lg">23</span>
+                            <h5 class=" mt-auto text-zinc-300 font-light">Kaijuu 8</h5>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
