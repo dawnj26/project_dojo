19f647b2bc6e7310e491e3324a1732d2eca38d1d
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-05-20 13:07:23 +0800

    doc: add api data struct

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,16 +8,69 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function showHome() {
+    public function showHome()
+    {
         return view('dashboard.home');
     }
 
-    public function showBrowse(Request $request) {
+    public function showBrowse(Request $request)
+    {
         $helper = new AnimeHelper();
 
         $animes = $helper->getAnime($request->search ?? '', 1);
+        // data structure of $animes
 
-        // dd($animes);
+        /*
+        array:1 [▼ // app\Http\Controllers\DashboardController.php:20
+        "data" => array:1 [▼
+            "Page" => array:2 [▼
+            "pageInfo" => array:5 [▶]
+            "media" => array:10 [▼
+                0 => array:7 [▼
+                "id" => 113415
+                "coverImage" => array:1 [▶]
+                "title" => array:1 [▶]
+                "description" => """
+                    A boy fights... for "the right death."<br>
+
+                    <br>
+
+
+        Hardship, regret, shame: the negative feelings that humans feel become Curses that lurk in our everyday lives. The Curses run rampant throughout the world, capa
+        ▶
+
+                    <br>
+
+
+        Itadori Yuji is a boy with tremendous physical strength, though he lives a completely ordinary high school life. One day, to save a friend who has been attacked
+        ▶
+
+                    <br><br>
+
+                    (Source: Crunchyroll)<br>
+
+                    <br>
+
+                    <i>Note: The first episode received an early web premiere on September 19th, 2020. The regular TV broadcast started on October 3rd, 2020.</i>
+                    """
+                "meanScore" => 86
+                "episodes" => 24
+                "genres" => array:3 [▶]
+                ]
+                1 => array:7 [▶]
+                2 => array:7 [▶]
+                3 => array:7 [▶]
+                4 => array:7 [▶]
+                5 => array:7 [▶]
+                6 => array:7 [▶]
+                7 => array:7 [▶]
+                8 => array:7 [▶]
+                9 => array:7 [▶]
+                    ]
+                ]
+            ]
+        ]
+        */
 
         return view('dashboard.browse', [
             'animes' => empty($animes) ? [] : $animes['data']['Page']['media']
